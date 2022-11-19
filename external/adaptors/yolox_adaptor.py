6b374a9c9e15bad1bf40730700ed1ec28736e9ba
Gerard <gerardmaggiolino@gmail.com>
2022-11-19 14:30:13 -0500

    YOLOX fix, very small .1 HOTA increase

--- external/adaptors/yolox_adaptor.py
+++ external/adaptors/yolox_adaptor.py
@@ -1,10 +1,12 @@
 import sys
+import warnings
 
 import torch
 import torch.nn as nn
 
 from yolox.models import YOLOPAFPN, YOLOX, YOLOXHead
 from yolox.utils import postprocess
+from yolox.models.network_blocks import BaseConv
 
 
 class PostModel(nn.Module):
@@ -24,10 +26,57 @@ class PostModel(nn.Module):
         return torch.cat((pred[:, :4], (pred[:, 4] * pred[:, 5])[:, None]), dim=1)
 
 
+def fuse_conv_and_bn(conv, bn):
+    # Fuse convolution and batchnorm layers https://tehnokv.com/posts/fusing-batchnorm-and-conv/
+    fusedconv = (
+        nn.Conv2d(
+            conv.in_channels,
+            conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            groups=conv.groups,
+            bias=True,
+        )
+        .requires_grad_(False)
+        .to(conv.weight.device)
+    )
+
+    # prepare filters
+    w_conv = conv.weight.clone().view(conv.out_channels, -1)
+    w_bn = torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
+    fusedconv.weight.copy_(torch.mm(w_bn, w_conv).view(fusedconv.weight.shape))
+
+    # prepare spatial bias
+    b_conv = (
+        torch.zeros(conv.weight.size(0), device=conv.weight.device)
+        if conv.bias is None
+        else conv.bias
+    )
+    b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(
+        torch.sqrt(bn.running_var + bn.eps)
+    )
+    fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
+
+    return fusedconv
+
+
+def fuse_model(model):
+
+    for m in model.modules():
+        if type(m) is BaseConv and hasattr(m, "bn"):
+            m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
+            delattr(m, "bn")  # remove batchnorm
+            m.forward = m.fuseforward  # update forward
+    return model
+
+
 def get_model(path):
     model = Exp().get_model()
     ckpt = torch.load(path)
     model.load_state_dict(ckpt["model"])
+    with warnings.catch_warnings():
+        model = fuse_model(model)
     model = PostModel(model)
     model.cuda()
     model.eval()
