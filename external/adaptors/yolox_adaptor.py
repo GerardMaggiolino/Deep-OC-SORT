6b6acca68e391217ed65ec12a3972dfc1b9940d5
Gerard <gerardmaggiolino@gmail.com>
2022-11-22 02:34:27 -0500

    Fixed regression

--- external/adaptors/yolox_adaptor.py
+++ external/adaptors/yolox_adaptor.py
@@ -62,7 +62,6 @@ def fuse_conv_and_bn(conv, bn):
 
 
 def fuse_model(model):
-
     for m in model.modules():
         if type(m) is BaseConv and hasattr(m, "bn"):
             m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
@@ -75,8 +74,8 @@ def get_model(path):
     model = Exp().get_model()
     ckpt = torch.load(path)
     model.load_state_dict(ckpt["model"])
-    with warnings.catch_warnings():
-        model = fuse_model(model)
+    # with warnings.catch_warnings():
+    #    model = fuse_model(model)
     model = PostModel(model)
     model.cuda()
     model.eval()
